26bd1a8b02badf7ec968fef69b91e1eefa311d79
Balanjani kamasani <40666589+Balanjani@users.noreply.github.com>
2023-11-01 22:02:10 -0500

    Updates board files

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var watchesRouter = require('./routes/watches');
+var boardRouter = require('./routes/board');
+var chooseRouter = require('./routes/choose');
+
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +26,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/watches', watchesRouter);
+app.use('/board', boardRouter);
+app.use('/choose', chooseRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
